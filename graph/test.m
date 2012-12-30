117068775d7229c78e4fbb7352ddd5470de7374d
cloudygoose <cloudygooseg@gmail.com>
2012-12-30 15:32:30 +0800

    no

--- graph/test.m
+++ graph/test.m
@@ -1,7 +1,16 @@
-cm = [0 1 1 0 0;1 0 0 1 1;1 0 0 0 0;0 0 0 0 1;1 0 1 0 0];
-ids = {'1','2','3','4','5'};
-bg2 = biograph(cm,ids)
-set(bg2.nodes, 'Shape', 'circle');
-set(bg2.edges, 'LineColor', [0, 0, 0]);
-set(bg2, 'ArrowSize', 4);
-view(bg2)
+clear,clc
+clf
+
+cm = [0 1 0 0;1 0 1 1;1 0 0 1;0 0 0 0];        %邻接矩阵
+bg = biograph(cm,'','NodeAutoSize','off','ShowTextInNodes','none','ArrowSize',6);
+
+pos = [0,100;0,0;100,0;100,100];               %节点位置坐标，pos的每一行对应cm里每一行的点
+
+dolayout(bg);
+set(bg.nodes,'Shape','circle','Color',[0 0 1],...
+    'LineColor',[0 0 0],'Linewidth',2,...
+    {'Position'},mat2cell(pos,[1,1,1,1],2));
+set(bg.edges,'LineColor',[0 0 0])
+dolayout(bg,'Pathsonly',1)
+
+view(bg)
