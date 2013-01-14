7633fe11494a9bb0e7f2a923c603e094d66d109f
cloudygoose <cloudygoose@126.com>
2013-01-14 17:07:17 +0800

    aaa

--- gradient/fyc.m
+++ gradient/fyc.m
@@ -1,38 +1,35 @@
 %This is a gradient descent for Figure 9.3
-function fyc()
-graddescbt('fd', 'dfd', [1 1], 0.1, 0.01, 0.1, 0.7)
-%[X,Y] = meshgrid(-2:.2:2,-2:.2:3);
-%Z = exp(X+3.*Y-0.1)+exp(X-3.*Y-0.1)+exp(-X-0.1);
-%[C,h] = contour(X,Y,Z);
+function [seq, len] = fyc()
+[seq, len] = graddescbt('fd', 'dfd', [1 1], 0.1, 0.01, 0.1, 0.7)
 [X,Y] = meshgrid(-0.8:0.01:0.2,-0.2:0.01:0.2);
 Z = exp(X+3.*Y-0.1)+exp(X-3.*Y-0.1)+exp(-X-0.1);
-%Z = 1 / 2 * (X .* X + 5 .* Y .* Y);
-%[C,h] = contour(X,Y,Z, 20);
-%Z = X.*exp(-X.^2-Y.^2);
-%[C,h] = contour(X,Y,Z, 50);
+figure
 contour(X, Y, Z, 10)
-%set(h,'ShowText','on','TextStep',get(h,'LevelStep')*2)
-%colormap cool
+figure
+plot(0, 0, 'o')
 end
 
-function soln = graddescbt(f, g, i, e, t, alpha, beta)
+function [seq, len] = graddescbt(f, g, x, e, t, alpha, beta)
 % gradient descent
 % f -- function
 % g -- gradient
-% i -- initial guess
+% x -- initial guess
 % e -- initial step size
 % t -- tolerance
-gi = feval(g,i) ;
-while(norm(gi)>t)  % crude termination condition
+gx = feval(g,x) ;
+seq = zeros(1000, 2);
+len = 1;
+seq(len, :) = x;
+while(norm(gx)>t)  % crude termination condition  norm is 2-norm
   step = e .* alpha;
-  while(feval(f,i - step .* feval(g,i)) > feval(f,i - step .* feval(g,i) .* beta))
+  while(feval(f,x - step .* feval(g,x)) > feval(f,x - step .* feval(g,x) .* beta))
       step = step .* beta;
   end
-  i = i - step .* feval(g,i) ;
-  i
- gi = feval(g,i) 
+  x = x - step .* feval(g,x) ;
+  len = len + 1;
+  seq(len, :) = x;
+  gx = feval(g,x); 
 end
-soln = i ;
 end
 
 function rtn= fd(c)
